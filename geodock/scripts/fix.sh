42519b9a0176cba0f0be1bad53867684ae0fe4ad
lchu11 <lchu11@jhu.edu>
2023-07-03 22:58:37 -0400

    clean up

--- geodock/scripts/fix.sh
+++
@@ -1,6 +0,0 @@
-for f in ../pdbs_400/*.pdb
-do 
-    name=$(basename $f)
-    filename=${name:0:4}_b.pdb
-    /home/lchu11/scr4_jgray21/lchu11/DockQ/scripts/fix_numbering.pl $f /home/lchu11/scr4_jgray21/lchu11/Docking-dev/data/db5_native/$filename
-done
--- geodock/scripts/run.sh
+++
@@ -1,4 +0,0 @@
-for i in 0 1 2 3
-do
-    python predict.py --test_all --count $i --dataset db5_test_unbound
-done
